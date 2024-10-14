0e664d9f65a82a1b95beee48a9e8a38ddd6aeaa4
xJam-byte <abylaimuratbayev@gmail.com>
2024-10-14 21:13:32 +0500

    cors options

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.enableCors({
-    origin: "http://localhost:3000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    credentials: true,
-  });
+
 
   await app.listen(PORT, () => {
     log("Service started on port " + PORT);
